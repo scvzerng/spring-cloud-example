20782dc504a659407860fbd69394ffaeec6269e6
scvzerng <842494771@qq.com>
2017-10-07 23:42:55 +0800

    api导出

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # 脚本名称
-SCRIPT="${0}"
+SCRIPT="$0"
 # 项目名称
 PROJECT="${1//_/-}"
 # 项目目录
-PROJECT_DIR="${1}"
+PROJECT_DIR="$1"
 # 启动环境
-PROFILE="${2}"
-
+PROFILE="$2"
+COMMAND="$3"
 # 根目录编译路径预处理 用于清除上次残留
 function prepareBuildDir(){
   if [  -d "target" ]; then
@@ -26,6 +26,11 @@ function exportGradleBuild(){
   echo "******copy project jar ${projectJar} to target success******"
 }
 
+function getServicePid(){
+  local pids=`ps -C java -f --width 1000 | grep "$projectJar" | awk '{print $2}'`
+  echo "$pids"
+}
+
 # 预处理并导出jar包到根目录
 function buildAndCopy(){
 prepareBuildDir
@@ -33,14 +38,27 @@ exportGradleBuild
 }
 # 直接运行服务
 function serviceRun(){
-PIDS=`ps -C java -f --width 1000 | grep "$projectJar" | awk '{print $2}'`
-if [ -n PIDS ];then
-echo "service ${PROJECT} is started width $PIDS please stop first"
-exit 1
+ local pids=$(getServicePid)
+ if [ pids ];then
+ echo "service ${PROJECT} is started width $pids please stop first"
+ exit 1
 fi
 
 projectJar=`find "${PROJECT_DIR}/build/libs/" -name *.jar`
 java -jar "${projectJar}" "--spring.profiles.active=${PROFILE}"
+}
+
+function stopService(){
+local pids=$(getServicePid)
+if [ !pids ];then
+echo "service ${PROJECT} is not running"
+exit 1
+
+
+
+fi
+
+kill -PIDS
 }
 
 echo -e "******script:${SCRIPT}****** \n ******project:${PROJECT}****** \n ******project_dir:${projectDir}******"
@@ -48,8 +66,8 @@ cd ..
 workspace=`pwd`
 echo "******root_project:${workspace}******"
 buildAndCopy
-if [ PROFILE ];then
-echo "******project start use ${PROFILE}******"
+if [ COMMAND ];then
+echo "******project $COMMAND use ${PROFILE}******"
 serviceRun
 fi
 echo "******${PROJECT} project build success******"
