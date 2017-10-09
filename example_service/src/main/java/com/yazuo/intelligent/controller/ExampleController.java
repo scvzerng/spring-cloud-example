9a53f2d36bcbe02f49d4dff10318e3935d8ccaba
scvzerng <443683059a>
2017-10-09 15:23:52 +0800

    zipkin sleuth 追踪服务调用

--- example_service/src/main/java/com/yazuo/intelligent/controller/ExampleController.java
+++ example_service/src/main/java/com/yazuo/intelligent/controller/ExampleController.java
@@ -1,5 +1,7 @@
 package com.yazuo.intelligent.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 public class ExampleController {
+    Logger logger = LoggerFactory.getLogger(ExampleController.class);
     @Value("${test}")
     private String test;
     @GetMapping("/props")
     public String props(){
+        logger.debug("get props");
         return test;
     }
 }
--- test_service/src/main/java/com/yazuo/intelligent/controller/TestServiceController.java
+++ test_service/src/main/java/com/yazuo/intelligent/controller/TestServiceController.java
@@ -2,6 +2,9 @@ package com.yazuo.intelligent.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.yazuo.intelligent.clients.ExampleServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +13,14 @@ import javax.annotation.Resource;
 
 @RestController
 public class TestServiceController {
+    Logger logger = LoggerFactory.getLogger(TestServiceController.class);
+    @Resource
+    Tracer tracer;
     @Resource
     ExampleServiceClient client;
     @GetMapping
     public String getTestProp(){
+        logger.debug("test:service:tradeId:"+tracer.getCurrentSpan().traceIdString());
         return client.props();
     }
     @GetMapping("hystrix/{test}")
